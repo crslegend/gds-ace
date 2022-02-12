336cee60d7f070f22f0a79764e1924e806b8a3d4
risheng17 <risheng17@gmail.com>
2022-02-12 12:45:07 +0800

    add logic to shortening url

--- pages/api/shorten-url.js
+++ pages/api/shorten-url.js
@@ -1,5 +1,6 @@
 // Endpoint to handle shorten of URL
-// import prisma from "../../lib/prisma";
+import prisma from "../../lib/prisma.ts";
+import { nanoid } from "nanoid";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -12,11 +13,34 @@ export default async function handler(req, res) {
     const regex = new RegExp(expression);
 
     if (!regex.test(enteredURL)) {
-      res.status(400).json("Invalid URL entered!");
+      res.status(400).json("You have entered an invalid URL!");
       return;
     }
 
-    res.status(200).json({ name: "John Doe" });
+    const urlRequest = await prisma.ShortenURLRequest.findFirst({
+      where: { entered_url: enteredURL },
+    });
+
+    if (urlRequest) {
+      const shortenedURL = urlRequest["shortened_url"];
+      res.status(200).json(shortenedURL);
+    } else {
+      // according to Nano ID Collison calculator (https://zelark.github.io/nano-id-cc/)
+      // ~2 years needed, in order to have a 1% probability of at least one collision for 9 characters
+      const shortenedURL = nanoid(9);
+
+      try {
+        await prisma.ShortenURLRequest.create({
+          data: {
+            entered_url: enteredURL,
+            shortened_url: shortenedURL,
+          },
+        });
+        res.status(200).json(shortenedURL);
+      } catch (error) {
+        res.status(400).json("Something went wrong. Please try again!");
+      }
+    }
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
