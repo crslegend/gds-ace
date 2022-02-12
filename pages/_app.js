89363c81a567e4d10e3eea4f52be1af9d2adbf83
risheng17 <risheng17@gmail.com>
2022-02-12 11:20:41 +0800

    add snackbar provider to app

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,19 @@ import PropTypes from "prop-types";
 import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { Button } from "@material-ui/core";
+import { SnackbarProvider } from "notistack";
+
 import theme from "../styles/theme";
 
 const MyApp = (props) => {
   const { Component, pageProps } = props;
+
+  // add action to all snackbars
+  const notistackRef = React.createRef();
+  const onClickDismiss = (key) => () => {
+    notistackRef.current.closeSnackbar(key);
+  };
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -19,12 +28,26 @@ const MyApp = (props) => {
   return (
     <React.Fragment>
       <Head>
-        <title>ShortenLah</title>
+        <title>ShortenLah!</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       </Head>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
+        <SnackbarProvider
+          maxSnack={2}
+          ref={notistackRef}
+          autoHideDuration={500000}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          action={(key) => (
+            <Button onClick={onClickDismiss(key)} style={{ color: "#fff" }}>
+              Dismiss
+            </Button>
+          )}
+        >
+          <CssBaseline />
+          <Component {...pageProps} />
+        </SnackbarProvider>
       </ThemeProvider>
     </React.Fragment>
   );
