aa5c88c9cfd3aeaa68c4323d436563ae6db98004
risheng17 <risheng17@gmail.com>
2022-02-15 15:45:51 +0800

    align theme and css for get url page

--- pages/[shortenedURL]/index.js
+++ pages/[shortenedURL]/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Image from "next/image";
 import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Typography } from "@material-ui/core";
 
 import { getShortenURL } from "../../helpers/controller";
-import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -14,15 +15,57 @@ const useStyles = makeStyles((theme) => ({
     gap: theme.spacing(3),
     backgroundColor: theme.palette.primary.main,
   },
+  backgroundImage: {
+    backgroundImage: `url(background.png)`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%",
+    padding: theme.spacing(3, 5),
+    gap: theme.spacing(2),
+    borderRadius: "15px",
+    textAlign: "center",
+    maxWidth: "500px",
+    [theme.breakpoints.down("sm")]: {
+      width: "80%",
+    },
+  },
+  headerSection: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    gap: theme.spacing(1),
+  },
+  header: {
+    fontWeight: 600,
+    background: `linear-gradient(to right, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+  },
 }));
 
 const ShortenedURLPage = (props) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.container}>
-      <Typography>logo here</Typography>
-      <Typography variant="h6">{`Can't seem to find the page leh!`}</Typography>
+    <div className={`${classes.container} ${classes.backgroundImage}`}>
+      <Paper className={classes.paper} elevation={5}>
+        <div className={classes.headerSection}>
+          <Image src={"/logo.png"} alt="logo" width={80} height={80} />
+          <Typography variant="h2" className={classes.header}>
+            ShortenLah!
+          </Typography>
+        </div>
+        <Typography variant="h6" style={{ fontWeight: 500 }}>{`We can't seem to find any page here!`}</Typography>
+      </Paper>
     </div>
   );
 };
