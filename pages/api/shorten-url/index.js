1367fd206b8955cd422a2ec7f94434f10bd2c645
risheng17 <risheng17@gmail.com>
2022-02-18 09:51:53 +0800

    add retry logic for collision

--- pages/api/shorten-url/index.js
+++ pages/api/shorten-url/index.js
@@ -27,19 +27,27 @@ export default async function handler(req, res) {
       res.status(200).json(shortenedURL);
     } else {
       // according to Nano ID Collison calculator (https://zelark.github.io/nano-id-cc/)
-      // ~2 years needed, in order to have a 1% probability of at least one collision for 9 characters
-      const shortenedURL = nanoid(9);
+      // for 1000 IDs generated per hour, ~12 days needed
+      // in order to have a 1% probability of at least one collision for 7 characters
+      let shortenedURL;
 
-      try {
-        await await db.collection("ShortenURLRequest").insertOne({
-          entered_url: enteredURL,
-          shortened_url: shortenedURL,
-        });
+      // try a maximum of 7 times if there is collision
+      for (let i = 0; i < 7; i++) {
+        shortenedURL = nanoid(7);
+        try {
+          await db.collection("ShortenURLRequest").insertOne({
+            entered_url: enteredURL,
+            shortened_url: shortenedURL,
+          });
 
-        res.status(200).json(shortenedURL);
-      } catch (error) {
-        res.status(400).json("Something went wrong. Please try again!");
+          res.status(200).json(shortenedURL);
+          return;
+        } catch (error) {
+          console.log(error);
+          // continue;
+        }
       }
+      res.status(400).json("Something went wrong. Please try again!");
     }
   } else {
     res.setHeader("Allow", "POST");
